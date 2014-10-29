0cd0f7832e8c574e83b9b42fb52678a3629c9af0
Pete LePage <pete.lepage@pobox.com>
2014-10-29 18:52:18 -0400

    initial work to get this to Google JS style guide.

--- src/commontrans.js
+++ src/commontrans.js
@@ -1,14 +1,17 @@
-var fs = require('fs');
-var path = require("path");
-var YAML = require("js-yaml");
-var marked = require("marked");
-var under = require("underscore");
-var md5 = require("blueimp-md5");
+/* jshint strict: true, node:true, browser:false */
 
+'use strict';
+
+var md5 = require('blueimp-md5');
+var YAML = require('js-yaml');
+var fs = require('fs');
+var marked = require('marked');
+var path = require('path');
+var under = require('underscore');
 
 if (!String.prototype.endsWith) {
   Object.defineProperty(String.prototype, 'endsWith', {
-    value: function (searchString, position) {
+    value: function(searchString, position) {
       var subjectString = this.toString();
       if (position === undefined || position > subjectString.length) {
         position = subjectString.length;
@@ -22,8 +25,10 @@ if (!String.prototype.endsWith) {
 
 function writeArbs(filename, obj) {
   under.each(obj, function(val, key) {
-    if (typeof val === "string") {
-      obj[key] = val.replace(/(<.+?>)/g, "{@$1}");
+    if (typeof val === 'string') {
+      val = val.replace(/<code>(.+?)<\/code>/g, '{@code>$1<code}');
+      val = val.replace(/(<.+?>)/g, '{@$1}');
+      obj[key] = val;
     }
   });
   fs.writeFileSync(filename, JSON.stringify(obj, null, 2));
@@ -33,8 +38,10 @@ function readArbs(filename) {
   var arbs = fs.readFileSync(filename);
   arbs = JSON.parse(arbs.toString());
   under.each(arbs, function(val, key) {
-    if (typeof val === "string") {
-      arbs[key] = val.replace(/{@(<.+?>)}/g , "$1");
+    if (typeof val === 'string') {
+      val = val.replace(/\{@(<.+?>)\}/g, '$1');
+      val = val.replace(/\{@code>(.+?)<\/code\}/g, '<code>$1</code>');
+      arbs[key] = val;
     }
   });
   return arbs;
@@ -42,10 +49,15 @@ function readArbs(filename) {
 
 
 var yamlKeys = [
-  "title", "description", "introduction", "key-takeaways", "notes", "remember"
+  'title',
+  'description',
+  'introduction',
+  'key-takeaways',
+  'notes',
+  'remember'
 ];
 
-// Parses the HTML and extracts any responsive table headers 
+// Parses the HTML and extracts any responsive table headers
 // (data-th="something"), then hashes the entire string and stores the value
 // for translation.
 function extractResponsiveTableHeaders(html) {
@@ -65,9 +77,9 @@ function replaceResponsiveTableHeaders(html, arb) {
   under.each(headers, function(header) {
     var hash = md5.md5(header);
     var str = arb[hash];
-    if (typeof str === "string") {
-      var re = new RegExp("data-th=\"" + header + "\"", "g");
-      html = html.replace(re, "data-th=\"" + str + "\"");
+    if (typeof str === 'string') {
+      var re = new RegExp('data-th="' + header + '"', 'g');
+      html = html.replace(re, 'data-th="' + str + '"');
     }
   });
   return html;
@@ -96,11 +108,11 @@ function extractStringsFromYaml(yaml) {
 
 function recurseYaml(yaml, callback, allChildren) {
   under.each(yaml, function(val, key) {
-    if (typeof val === "string") {
+    if (typeof val === 'string') {
       if ((yamlKeys.indexOf(key) >= 0) || (allChildren === true)) {
         callback(key, val, yaml);
       }
-    } else if (typeof val === "object") {
+    } else if (typeof val === 'object') {
       if (yamlKeys.indexOf(key) >= 0) {
         recurseYaml(val, callback, true);
       } else {
@@ -111,7 +123,7 @@ function recurseYaml(yaml, callback, allChildren) {
 }
 
 function recurseDir(dir, rootDir, callback) {
-  if (dir.indexOf("_code") === -1) {
+  if (dir.indexOf('_code') === -1) {
     var files = fs.readdirSync(dir);
     for (var i = 0; i < files.length; i++) {
       var file = path.join(dir, files[i]);
@@ -119,7 +131,7 @@ function recurseDir(dir, rootDir, callback) {
       if (stat.isDirectory()) {
         recurseDir(file, rootDir, callback);
       } else {
-        if (file.indexOf("DS_Store") === -1) {
+        if (file.indexOf('DS_Store') === -1) {
           callback(file);
         }
       }
@@ -133,55 +145,57 @@ function recurseDir(dir, rootDir, callback) {
 
 */
 
-var standardString = "\n\n<div mdx id=\"WonderLand\">Down the rabbit hole</div>";
+var standardString = '\n\n<div mdx id="WonderLand">Down the rabbit hole</div>';
 
 var renderer = new marked.Renderer();
 
 // Block level elements
 renderer.code = function(code) {
-  return "<!-- L10N_TODO: Check code spacing -->\n" + code + "\n\n";
+  return '<!-- L10N_TODO: Check code spacing -->\n' + code + '\n\n';
 };
 renderer.heading = function(text, level) {
   var id = text.toLowerCase().replace(/[^\w]+/g, '-');
-  return "<h" + level + " id=\"" + id + "\" mdx>" + text + "</h" + level + ">\n\n";
+  var result = '<h' + level + ' id=\"' + id + '\" mdx>' +
+    text + '</h' + level + '>\n\n';
+  return result;
 };
 renderer.list = function(body, ordered) {
   if (ordered === true) {
-    body = "<ol mdx>\n" + body.replace(/mdli/g, "mdol") + "</ol>\n\n";
-  } else  {
-    body = "<ul mdx>\n" + body.replace(/mdli/g, "mdul") + "</ul>\n\n";
+    body = '<ol mdx>\n' + body.replace(/mdli/g, 'mdol') + '</ol>\n\n';
+  } else {
+    body = '<ul mdx>\n' + body.replace(/mdli/g, 'mdul') + '</ul>\n\n';
   }
   return body;
 };
 renderer.listitem = function(text) {
-  return "  <li mdli>" + text + "</li>\n";
+  return '  <li mdli>' + text + '</li>\n';
 };
 renderer.paragraph = function(text) {
-  return text + "\n\n";
+  return text + '\n\n';
 };
 
 // Inline elements
 renderer.link = function(href, title, text) {
-  return "<a mdx href=\"" + href + "\">" + text + "</a>";
+  return '<a mdx href=\"' + href + '\">' + text + '</a>';
 };
 renderer.strong = function(text) {
-  return "<b mdx>" + text + "</b>";
+  return '<b mdx>' + text + '</b>';
 };
 renderer.em = function(text) {
-  return "<u mdx>" + text + "</u>";
+  return '<u mdx>' + text + '</u>';
 };
 renderer.codespan = function(text) {
-  return "<code class=\"notranslate\">" + text + "</code>";
+  return '<code class=\"notranslate\">' + text + '</code>';
 };
 renderer.image = function(href, title, text) {
-  var result = "<img src=\"" + href + "\"";
+  var result = '<img src=\"' + href + '\"';
   if (text) {
-    result += " alt=\"" + text + "\"";
+    result += ' alt=\"' + text + '\"';
   }
   if (title) {
-    result += " title=\"" + title + "\"";
+    result += ' title=\"' + title + '\"';
   }
-  return result + ">";
+  return result + '>';
 };
 
 function markdownToHTML(content) {
@@ -189,44 +203,57 @@ function markdownToHTML(content) {
   return html;
 }
 
+function replaceAllUsingRegEx(str, regexes) {
+  var result = str;
+  for (var i = 0; i < regexes.length; i++) {
+    var re = new RegExp(regexes[i][0], 'g');
+    result = result.replace(re, regexes[i][1]);
+  }
+  return result;
+}
+
 function wrapJekyllTags(html) {
-  html = html.replace(/({%.*%})/g, "<div class=\"notranslate\">$1</div>");
-  html = html.replace(/({% highlight .* %})<\/div>/g, "$1");
-  html = html.replace(/<div class=\"notranslate\">({% endhighlight %}<\/div>)/g, "$1");
-  html = html.replace(/({% comment %})<\/div>/g, "$1");
-  html = html.replace(/<div class=\"notranslate\">({% endcomment %}<\/div>)/g, "$1");
-  return html;
+  var regexes = [
+    ['({%.*%})', '<div class="notranslate">$1</div>'],
+    ['({% highlight .* %})</div>', '$1'],
+    ['<div class="notranslate">({% endhighlight %}</div>)', '$1'],
+    ['({% comment %})</div>', '$1'],
+    ['<div class="notranslate">({% endcomment %}</div>)', '$1']
+  ];
+  return replaceAllUsingRegEx(html, regexes);
 }
 
 function htmlToMarkdown(html) {
-  html = html.replace(/\n\n<div id=\"WonderLand\" mdx=\"\">(.+?)<\/div>/g, "");
-  html = html.replace(/<div class="notranslate">(.+?)<\/div>/g, "$1");
-  html = html.replace(/<[o|u]l mdx=\"\">([\S\s]+?)<\/[o|u]l>/g, "$1");
-  html = html.replace(/<li mdol=\"\">([\S\s]+?)<\/li>/g, "1. $1");
-  html = html.replace(/<li mdul=\"\">([\S\s]+?)<\/li>/g, "* $1");
-  html = html.replace(/<h1(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h1>/g, "# $2 {#$1}");
-  html = html.replace(/<h2(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h2>/g, "## $2 {#$1}");
-  html = html.replace(/<h3(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h3>/g, "### $2 {#$1}");
-  html = html.replace(/<h4(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h4>/g, "#### $2 {#$1}");
-  html = html.replace(/<h5(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h5>/g, "##### $2 {#$1}");
-  html = html.replace(/<h6(?: id=\"([\S\s]+?)\")? mdx=\"\">(.+?)<\/h6>/g, "###### $2 {#$1}");
-  html = html.replace(/<code class=\"notranslate\">(.+?)<\/code>/g, "`$1`");
-  html = html.replace(/<b mdx=\"\">(.+?)<\/b>/g, "**$1**");
-  html = html.replace(/<u mdx=\"\">(.+?)<\/u>/g, "*$1*");
-  html = html.replace(/<div class=\"notranslate\">({% highlight.+?%})/g, "$1");
-  html = html.replace(/({% endhighlight %})<\/div>/g, "$1");
-  html = html.replace(/<div class=\"notranslate\">({% comment %})/g, "$1");
-  html = html.replace(/({% endcomment %})<\/div>/g, "$1");
-  // html = html.replace(/<a href=\"([\S\s]+?)\"(?: title=\"([\S\s]+?)\")?? mdx=\"\">([\S\s]+?)<\/a>/g, "[$3]($1)");
-  html = html.replace(/(&quot;)+?(?=[\s\S]+?%})/g, "\"");
-  return html;
+  var regexes = [
+    ['\\n\\n<div id="WonderLand" mdx="">(.+?)</div>', ''],
+    ['<div class="notranslate">(.+?)</div>', '$1'],
+    ['<[o|u]l mdx="">([\\S\\s]+?)</[o|u]l>', '$1'],
+    ['<li mdol="">([\\S\\s]+?)</li>', '1. $1'],
+    ['<li mdul="">([\\S\\s]+?)</li>', '* $1'],
+    ['<h1(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h1>', '# $2 {#$1}'],
+    ['<h2(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h2>', '## $2 {#$1}'],
+    ['<h3(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h3>', '### $2 {#$1}'],
+    ['<h4(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h4>', '#### $2 {#$1}'],
+    ['<h5(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h5>', '##### $2 {#$1}'],
+    ['<h6(?: id="([\\S\\s]+?)")? mdx="">(.+?)</h6>', '###### $2 {#$1}'],
+    ['<code class="notranslate">(.+?)</code>', '`$1`'],
+    ['<b mdx="">(.+?)</b>', '**$1**'],
+    ['<u mdx="">(.+?)</u>', '*$1*'],
+    ['<div class="notranslate">({% highlight.+?%})', '$1'],
+    ['({% endhighlight %})</div>', '$1'],
+    ['<div class="notranslate">({% comment %})', '$1'],
+    ['({% endcomment %})</div>', '$1'],
+    ['(&quot;)+?(?=[\\S\\s]+?%})', '"']
+  ];
+  return replaceAllUsingRegEx(html, regexes);
 }
 
 function gttCleanup(html) {
-  var injectedHeader = "<html dir=\"ltr\"><head></head><body dir=\"ltr\"><div class=\"notranslate\">\n";
+  var injectedHeader = '<html dir="ltr"><head></head><body dir="ltr">' +
+    '<div class="notranslate">\n';
   if (html.trim().indexOf(injectedHeader) === 0) {
-    html = html.replace(injectedHeader, "<div class=\"notranslate\">\n");
-    html = html.replace("---\n</div>\n", "---\n");
+    html = html.replace(injectedHeader, '<div class="notranslate">\n');
+    html = html.replace('---\n</div>\n', '---\n');
     html = html.substring(0, html.length - 14);
   }
   return html;
