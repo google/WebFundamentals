44c287c222dd6ede629a700ac3ddd8112674c46e
Paul Bakaus <pbakaus@chromium.org>
2015-01-20 17:16:46 +0100

    RSS feed for Web Fundamentals (excerpt for time being)

--- src/_plugins/filters.rb
+++ src/_plugins/filters.rb
@@ -1,15 +1,48 @@
 module Jekyll
   module DeepSortFilter
-	def deepsort(input, property = nil)
-      ary = InputIterator.new(input)
+
+	def deepsort(input, property = nil, deepProperty = nil)
+	  ary = InputIterator.new(input)
+	  ary = ary.find_all {|a| a[property] }
       if property.nil?
         ary.sort
-      elsif ary.first.respond_to?(:[]) && !ary.first[property].nil?
-        ary.sort {|a,b| a[property] <=> b[property] }
+      elsif ary.first.respond_to?(:[]) && !ary.first[property][deepProperty].nil?
+        ary.sort {|a,b| a[property][deepProperty] <=> b[property][deepProperty] }
       elsif ary.first.respond_to?(property)
         ary.sort {|a,b| a.send(property) <=> b.send(property) }
       end
     end
+
+    class InputIterator
+      include Enumerable
+
+      def initialize(input)
+        @input = if input.is_a?(Array)
+          input.flatten
+        elsif input.is_a?(Hash)
+          [input]
+        elsif input.is_a?(Enumerable)
+          input
+        else
+          Array(input)
+        end
+      end
+
+      def join(glue)
+        to_a.join(glue)
+      end
+
+      def reverse
+        reverse_each.to_a
+      end
+
+      def each
+        @input.each do |e|
+          yield(e.respond_to?(:to_liquid) ? e.to_liquid : e)
+        end
+      end
+    end
+
   end
 end
 
