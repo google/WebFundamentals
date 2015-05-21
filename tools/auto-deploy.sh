07e7931a30d99edee5020a11e6460be70c9dbd75
alex <alex@cloudware.io>
2015-05-21 20:26:31 +0100

    Switch from travis-ci to drone.io
    New build history page:
    https://drone.io/github.com/google/WebFundamentals

--- tools/auto-deploy.sh
+++ tools/auto-deploy.sh
@@ -6,7 +6,7 @@ CLOUDSDK_URL=https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
 SDK_DIR=google-cloud-sdk
 
 # deploy only master builds
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$DRONE_BRANCH" != "master" ]; then
   echo "Skip deploy."
   exit 0
 fi
@@ -21,7 +21,7 @@ if [ ! -d $SDK_DIR ]; then
   $SDK_DIR/install.sh
 fi
 
-openssl aes-256-cbc -d -K $encrypted_29722e8fb5df_key -iv $encrypted_29722e8fb5df_iv \
+openssl aes-256-cbc -d -k $KEY_PASSPHRASE \
         -in tools/web-central-44673aab0806.json.enc \
         -out tools/web-central-44673aab0806.json
 
@@ -29,4 +29,4 @@ $SDK_DIR/bin/gcloud components update gae-python -q
 $SDK_DIR/bin/gcloud auth activate-service-account $SERVICE_ACCOUNT \
         --key-file tools/web-central-44673aab0806.json \
 				--quiet
-$SDK_DIR/bin/appcfg.py --oauth2 -A web-central -V travis-master update ./appengine
+$SDK_DIR/bin/appcfg.py -A web-central -V master update ./appengine
