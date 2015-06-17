186e537449b5b09210a363aa95df5f14a81ff3e0
Pete LePage <pete.lepage@pobox.com>
2015-06-17 12:03:17 -0400

    Fixes #1373

--- src/_plugins/include_code.rb
+++ src/_plugins/include_code.rb
@@ -143,6 +143,7 @@ module Jekyll
       end
 
       relpath = filepath.sub("_langs/en/fundamentals/", "")
+      relpath = filepath.sub("_langs/en/tools/", "tools/")
 
       page = context.environments.first["page"]
       site = context.registers[:site]
@@ -155,7 +156,7 @@ module Jekyll
   </div>
   <div dir="ltr" class="highlight-module highlight-module--code highlight-module--right">
     <div class="highlight-module__container">
-      <code class='html'>#{highlighted_code.strip}</code>                                                     
+      <code class='html'>#{highlighted_code.strip}</code>                                                  
       <a class="highlight-module__cta button--primary" href="#{context.registers[:site].config["sample_link_base"]}#{relpath}">#{link_text}</a>
     </div>
   </div>
