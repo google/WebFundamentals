5fb57af7a778ec2ffad555db9f8ccb2b5d5fe9ff
Pete LePage <pete.lepage@pobox.com>
2015-06-24 13:39:10 +0100

    Copies required assets to samples

--- tools/publishSamples.sh
+++ tools/publishSamples.sh
@@ -51,19 +51,54 @@ if [[ ! -d $GHSAMPLESDIR ]]; then
   git clone -b gh-pages https://github.com/googlesamples/web-fundamentals.git $GHSAMPLESDIR
 fi
 
+echo "----- Getting latest bits"
 cd $GHSAMPLESDIR
 # Make sure we've got the latest bits
 git pull
 
+echo "----- Copying latest samples to $GHSAMPLESDIR"
 # Copy the samples from the build directory to the $GHSAMPLEDIR
 cp -r $WEBDIR/appengine/build/_langs/en/fundamentals/resources/samples/* $GHSAMPLESDIR/samples
 
+echo "----- Copying additional resource files"
+# Move the index file from samples to the root
+mv $GHSAMPLESDIR/samples/index.html $GHSAMPLESDIR/index.html
+
+# Copy the required assets from the build to their directories
+if [[ ! -d $GHSAMPLESDIR/web/ ]]; then
+  mkdir $GHSAMPLESDIR/web/
+fi
+cp $WEBDIR/appengine/build/favicon.ico $GHSAMPLESDIR/web/
+
+if [[ ! -d $GHSAMPLESDIR/web/css/ ]]; then
+  mkdir $GHSAMPLESDIR/web/css/
+fi
+cp $WEBDIR/appengine/build/css/styles.min.css $GHSAMPLESDIR/web/css/
+
+if [[ ! -d $GHSAMPLESDIR/web/icons/ ]]; then
+  mkdir $GHSAMPLESDIR/icons/
+fi
+cp $WEBDIR/appengine/build/icons/icons.* $GHSAMPLESDIR/web/icons/
+
+if [[ ! -d $GHSAMPLESDIR/web/imgs/ ]]; then
+  mkdir $GHSAMPLESDIR/web/imgs/
+fi
+cp $WEBDIR/appengine/build/imgs/developers-logo.svg $GHSAMPLESDIR/web/imgs/
+cp $WEBDIR/appengine/build/imgs/developers-logo_short.svg $GHSAMPLESDIR/web/imgs/
+
+
 # Commit and push changes up to repo
+echo "----- Adding new files"
 git add .
+echo "----- Commiting to repo"
 git commit -a -m "Updating Web Fundamentals Samples to $LASTCOMMIT"
+echo "----- Pushing to gh-pages"
 git push origin gh-pages
 
 popd
+echo "----- Done!"
+echo ""
+echo ""
 
 echo "----- Samples pushed live to https://github.com/googlesamples/web-fundamentals/tree/gh-pages
   View the samples at: https://googlesamples.github.io/web-fundamentals/samples/
