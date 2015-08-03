fbbe061a3e07bdcab1ba662c5c95b15937475a56
Matt Gaunt <matt@gauntface.co.uk>
2015-08-03 16:20:20 +0100

    Switcihng tests to cable

--- tools/bigrig/config.js
+++ tools/bigrig/config.js
@@ -8,8 +8,8 @@ module.exports = {
     'Fundamentals Home Page (3G, Motorola G)': {
       'url': rootUrl + 'fundamentals/',
       'labels': 'fundamentals-home-page-3g-motorola-g',
-      'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
-      'connectivity': '3G'
+      //'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
+      //'connectivity': '3G'
     },
 
     // Tools
@@ -22,14 +22,14 @@ module.exports = {
     'Updates Home Page (3G, Motorola G)': {
       'url': rootUrl + 'updates/',
       'labels': 'updates-home-page-3g-motorola-g',
-      'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
-      'connectivity': '3G'
+      //'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
+      //'connectivity': '3G'
     },
     'Updates Article (3G, Motorola G)': {
       'url': rootUrl + 'updates/2015/06/mse-gapless-audio.html',
       'labels': 'updates-article-page-3g-motorola-g',
-      'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
-      'connectivity': '3G'
+      //'location': 'Dulles_MotoG:Motorola G - Chrome Dev',
+      //'connectivity': '3G'
     },
 
   }
