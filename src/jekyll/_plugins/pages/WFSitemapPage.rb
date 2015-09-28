45d306c7914a49a3e9988f7c7d8848df1f46b9fd
Matt Gaunt <matt@gauntface.co.uk>
2015-09-28 15:12:48 +0100

    Adding sitemap support - fixes #1884

---
+++ src/jekyll/_plugins/pages/WFSitemapPage.rb
@@ -0,0 +1,73 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Jekyll
+
+  class WFSitemapPage < LanguagePage
+
+    def initialize(site, pages)
+      sitemapLayout = 'shared/sitemap.liquid';
+      filename = 'sitemap.xml';
+
+      super(site, '', filename, 'en')
+
+      self.data = self.data ? self.data : {}
+
+      sitemapPages = pages.reject { |page|
+        reject = false
+
+        if page.nil?
+          reject = true
+        else
+          if (!page.data['published'].nil?) && (page.data['published']) != true
+            reject = true
+          end
+        end
+
+        reject
+      }
+
+      sitemapPages = sitemapPages.sort do |a, b|
+        # TODO: this should be a shared method between here and build-end-generator
+        a_order = 0
+        b_order = 0
+
+        if !a['index'].nil?
+          a_order = a['index'].data['order'] || a['index'].data['date'] || heavy_weight
+        end
+        if !b['index'].nil?
+          b_order = b['index'].data['order'] || b['index'].data['date'] || heavy_weight
+        end
+
+        if a_order.is_a?(Integer) & b_order.is_a?(Integer)
+            a_order <=> b_order
+        elsif a_order.is_a?(String) & b_order.is_a?(String)
+            a_order <=> b_order
+        else
+          0 <=> 0
+        end
+      end
+
+      self.data['sitemap_pages'] = sitemapPages
+
+      # This will read the liquid file and asign the page the appropriate content
+      self.read_yaml(File.join(site.source, '_layouts'), sitemapLayout)
+    end
+
+    def autogenerateBetterBook()
+      # NOOP - Sitemap doesn't need a menu
+    end
+  end
+
+end
