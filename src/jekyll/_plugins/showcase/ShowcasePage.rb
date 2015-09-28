85fb7d2f60c0fd9ecc30cd1d33e7b471f0371c59
Pete LePage <pete.lepage@pobox.com>
2015-09-28 13:00:08 +0100

    Fixes #1884

--- src/jekyll/_plugins/showcase/ShowcasePage.rb
+++ src/jekyll/_plugins/showcase/ShowcasePage.rb
@@ -23,7 +23,7 @@ module Jekyll
     DEFAULT_HEAD_DESCRIPTION = 'Google Web Showcase - Google Developers'
 
     def initialize(site, relativeDir, filename, langcode)
-      validKeys = ['tags', 'showcase']
+      validKeys = ['tags', 'showcase', 'subtitle', 'featured_image']
       super(site, relativeDir, filename, langcode, validKeys)
 
       self.data['drawerTitleText'] = 'Web Showcase'
