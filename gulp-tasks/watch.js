2bdaf3abb5c83786ae21bc86623f7416c7e35eba
Matt Gaunt <matt@gauntface.co.uk>
2015-10-01 16:14:52 +0100

    Removing the google verification and adding minification to CSS and HTML

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -4,17 +4,25 @@ var gulp = require('gulp');
 require('gulp-watch');
 
 gulp.task('dev-watch-tasks', function() {
-  try {
-    gulp.watch([GLOBAL.WF.src.styles + '/**/*.scss',
-      GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css']);
-    gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts']);
-    gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images']);
-    gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
-    gulp.watch([
-      GLOBAL.WF.src.jekyll + '/**/*',
-      GLOBAL.WF.src.content + '/**/*'],
-      ['compile-jekyll:localhost']);
-  } catch (exception) {
-    console.log('<----------------Trying to catch errors in watch task');
-  }
+  gulp.watch([GLOBAL.WF.src.styles + '/**/*.scss',
+    GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css']);
+  gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts']);
+  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images']);
+  gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
+  gulp.watch([
+    GLOBAL.WF.src.jekyll + '/**/*',
+    GLOBAL.WF.src.content + '/**/*'],
+    ['compile-jekyll:localhost']);
+});
+
+gulp.task('prod-watch-tasks', function() {
+  gulp.watch([GLOBAL.WF.src.styles + '/**/*.scss',
+    GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css']);
+  gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts']);
+  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images']);
+  gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
+  gulp.watch([
+    GLOBAL.WF.src.jekyll + '/**/*',
+    GLOBAL.WF.src.content + '/**/*'],
+    ['compile-jekyll:localhost', 'html']);
 });
