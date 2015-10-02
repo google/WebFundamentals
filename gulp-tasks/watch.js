0d94c75a932e179bf3740da3d25380a82fee6159
Matt Gaunt <matt@gauntface.co.uk>
2015-10-02 13:18:44 +0100

    Adding browser sync, minor changed to youtube embed and page content margins

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -2,18 +2,37 @@
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
 require('gulp-watch');
 
 gulp.task('dev-watch-tasks', function() {
+  console.log('Starting up BrowserSync');
+  browserSync.init({
+    proxy: 'localhost:7331',
+    logPrefix: 'GF',
+    // Prevent browser sync from display in page notifications
+    notify: false,
+    open: false
+  });
+
   gulp.watch([GLOBAL.WF.src.styles + '/**/*.scss',
-    GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css']);
-  gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts']);
-  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images']);
-  gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
+    GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css'])
+      .on('change', browserSync.reload);
+
+  gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts'])
+    .on('change', browserSync.reload);
+
+  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images'])
+    .on('change', browserSync.reload);
+
   gulp.watch([
     GLOBAL.WF.src.jekyll + '/**/*',
     GLOBAL.WF.src.content + '/**/*'],
-    ['compile-jekyll:localhost']);
+    ['compile-jekyll:localhost'])
+    .on('change', browserSync.reload);
+
+  gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
 });
 
 gulp.task('prod-watch-tasks', function() {
