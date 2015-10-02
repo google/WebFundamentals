81442bc58d8a270aeb7d56d7fc783dfdbdf42ff4
Matt Gaunt <matt@gauntface.co.uk>
2015-10-02 11:46:01 +0100

    Fixed up the build to minify assets on production builds - SVGs are breaking in some places though

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -20,11 +20,11 @@ gulp.task('prod-watch-tasks', function() {
   gulp.watch([GLOBAL.WF.src.styles + '/**/*.scss',
     GLOBAL.WF.src.thirdParty + '/**/*.scss'], ['generate-dev-css']);
   gulp.watch([GLOBAL.WF.src.scripts + '/**/*.js'], ['cp-scripts']);
-  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['cp-images']);
+  gulp.watch([GLOBAL.WF.src.imgs + '/**/*'], ['minify-images']);
   gulp.watch([GLOBAL.WF.gae + '/**/*'], ['copy-appengine-config']);
   gulp.watch([
     GLOBAL.WF.src.jekyll + '/**/*',
     GLOBAL.WF.src.content + '/**/*']).on('change', function() {
-      runSequence('compile-jekyll:localhost', 'html');
+      runSequence('compile-jekyll:localhost', 'html', 'minify-images:content');
     });
 });
