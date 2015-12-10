b8dda4e22cf2470241ba056823b0deabd0000c62
alex <alex@cloudware.io>
2015-12-10 15:22:00 +0000

    Switch from Travis to private drone instance

--- tools/auto-deploy.sh
+++ tools/auto-deploy.sh
@@ -5,12 +5,6 @@ set -e
 CLOUDSDK_URL=https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
 SDK_DIR=google-cloud-sdk
 
-# deploy only master builds
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skip deploy."
-  exit 0
-fi
-
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
@@ -21,7 +15,7 @@ if [ ! -d $SDK_DIR ]; then
   $SDK_DIR/install.sh
 fi
 
-openssl aes-256-cbc -d -k $KEY_PASSPHRASE \
+openssl aes-256-cbc -d -k $SERVICE_SECRET \
         -in tools/web-central-44673aab0806.json.enc \
         -out tools/web-central-44673aab0806.json
 
@@ -34,6 +28,6 @@ $SDK_DIR/bin/gcloud auth activate-service-account $SERVICE_ACCOUNT \
 $SDK_DIR/bin/gcloud config set project web-central
 $SDK_DIR/bin/gcloud config set app/promote_by_default false
 $SDK_DIR/bin/gcloud config set app/use_appengine_api true
-$SDK_DIR/bin/gcloud preview app modules cancel-deployment default --version $TRAVIS_BRANCH
-$SDK_DIR/bin/gcloud preview app deploy --version $TRAVIS_BRANCH --bucket gs://${TRAVIS_BRANCH}-webcentral-appspot-com  ./build/app.yaml
+$SDK_DIR/bin/gcloud preview app modules cancel-deployment default --version $CI_BRANCH
+$SDK_DIR/bin/gcloud preview app deploy --version $CI_BRANCH --bucket gs://${CI_BRANCH}-webcentral-appspot-com  ./build/app.yaml
 
