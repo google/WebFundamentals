f461922a8534d5ebcc9fb970f69464c97f735061
Pete LePage <pete.lepage@pobox.com>
2016-08-19 15:15:03 -0400

    disable auto build of contributors

--- Pending/start-appengine.sh
+++ Pending/start-appengine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Building contributors files..."
-node build-contributors.js
+# echo "Building contributors files..."
+# node build-contributors.js
 
 echo "Starting server on: http://localhost:8080/"
 
