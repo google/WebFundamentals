7078c6491949aa97c43be5e3db0cf262f77a9a6b
Pete LePage <pete.lepage@pobox.com>
2016-08-22 11:44:39 -0400

    Build updates cleanup and todo

--- Pending/build-updates.js
+++ Pending/build-updates.js
@@ -1,16 +1,19 @@
 'use strict';
 
+/*
+    build-updates.js
+
+    Gets all of the updates and generates necessary index files, including:
+    homepage, tocs, year indexes, RSS and ATOM feeds.
+ */
+
 var fs = require('fs');
 var path = require('path');
 var moment = require('moment');
-var jsYaml = require('js-yaml');
 var Handlebars = require('handlebars');
 
+var MAX_ARTICLES_ON_HOMEPAGE = 15;
 var ROOT_PATH = './src/content';
-var JSYAML_OPTS = {
-  lineWidth: 1024
-};
-
 var RE_PUBLISHED = /{# wf_published_on: (.*?) #}/;
 var RE_DESCRIPTION = /^description: (.*)/m;
 var RE_TITLE = /^# (.*) {: .page-title }/m;
@@ -40,6 +43,45 @@ function getRegEx(regEx, content, defaultResponse) {
   return defaultResponse;
 }
 
+function getListOfUpdates(sourcePath) {
+  var result = [];
+  var files = fs.readdirSync(sourcePath);
+  files.forEach(function(file) {
+    if (file !== 'index.md' && file.endsWith('.md')) {
+      result.push(path.join(sourcePath, file));
+    } else {
+      var fileStat = fs.statSync(path.join(sourcePath, file));
+      if (fileStat.isDirectory()) {
+        result = result.concat(getListOfUpdates(path.join(sourcePath, file)));
+      }
+    }
+  });
+  return result;
+}
+
+function getMetadata(file) {
+  var result = {};
+  var content = fs.readFileSync(file, 'utf8');
+  result.title = getRegEx(RE_TITLE, content);
+  var url = '/web' + file.substring(file.indexOf('/updates'));
+  url = url.replace('.md', '');
+  result.path = url;
+  result.description = getRegEx(RE_DESCRIPTION, content);
+  result.author = getRegEx(RE_AUTHOR, content);
+  result.image = getRegEx(RE_IMAGE, content);
+  var published = moment(getRegEx(RE_PUBLISHED, content));
+  result.published = published.format('YYYY-MM-DD');
+  result.publishedYear = published.format('YYYY');
+  result.publishedMonth = published.format('MM');
+  var tags = getRegEx(RE_TAGS, content);
+  if (tags) {
+    result.tags = tags.split(',');
+  } else {
+    result.tags = [];
+  }
+  return result;
+}
+
 function compareTocDates(a, b) {
   var aPublished = moment(a.published);
   var bPublished = moment(b.published);
@@ -51,143 +93,99 @@ function compareTocDates(a, b) {
   return 0;
 }
 
-function compareInclude(a, b) {
-  if (a.include < b.include) {
-    return 1;
-  } else if (a.include > b.include) {
-    return -1;
-  }
-  return 0;
-}
-
-function buildMonth(lang, year, month) {
-  var toc = [];
-  var sourcePath = path.join(ROOT_PATH, lang, 'updates', year, month);
-  var articles = fs.readdirSync(sourcePath);
-  articles.forEach(function(file) {
-    if (file.endsWith('.md')) {
-      var url = '/web/updates/' + year + '/' + month + '/';
-      url += file.replace('.md', '');
-      var content = fs.readFileSync(path.join(sourcePath, file), 'utf8');
-      var title = getRegEx(RE_TITLE, content);
-      var published = getRegEx(RE_PUBLISHED, content);
-      var author = getRegEx(RE_AUTHOR, content);
-      var article = {
-        title: title,
-        published: published,
-        author: author,
-        path: url
-      };
-      var description = getRegEx(RE_DESCRIPTION, content);
-      if (description) {
-        article.description = description;
-      }
-      var tags = getRegEx(RE_TAGS, content);
-      if (tags) {
-        article.tags = tags.split(',');
-      }
-      var image = getRegEx(RE_IMAGE, content);
-      if (image) {
-        article.image = image;
-      }
-      toc.push(article);
+function getAllMetadata(fileList) {
+  console.log('Reading all update files...');
+  var result = {};
+  fileList.forEach(function(file) {
+    var article = getMetadata(file);
+    if (!result[article.publishedYear]) {
+      result[article.publishedYear] = [];
     }
+    result[article.publishedYear].push(article);
   });
-  toc.sort(compareTocDates);
-  return toc;
+  var keys = Object.keys(result);
+  keys.forEach(function(key) {
+    result[key].sort(compareTocDates);
+  });
+  return result;
 }
 
-function writeToc(articles, title, filename) {
-  var toc = [];
-  articles.forEach(function(article) {
-    var tocItem = {
-      title: article.title,
-      path: article.path
+function generateYearTOCs(metadata) {
+  console.log('Generating _toc.yaml files...');
+  var years = Object.keys(metadata);
+  var ts = fs.readFileSync('./src/templates/updates-toc.yaml', 'utf8');
+  var template = Handlebars.compile(ts);
+  years.forEach(function(year) {
+    console.log(' ', year);
+    var result = {
+      year: year,
+      months: {}
     };
-    toc.push(tocItem);
+    metadata[year].forEach(function(article) {
+      var month = moment.months()[parseInt(article.publishedMonth) - 1];
+      if (!result.months[month]) {
+        result.months[month] = [];
+      }
+      result.months[month].push(article);
+    });
+    var filename = path.join('./src/content/en/updates/', year, '_toc.yaml');
+    fs.writeFileSync(filename, template(result));
   });
-  var result = {
-    toc: [{
-      title: title,
-      section: toc
-    }]
-  };
-  fs.writeFileSync(filename, jsYaml.dump(result, JSYAML_OPTS));
 }
 
-function writeYearToc(includes, year, tocFile) {
-  includes.sort(compareInclude);
-  var result = {
-    toc: [{
-      title: year.toString(),
-      section: includes
-    }]
-  };
-  fs.writeFileSync(tocFile, jsYaml.dump(result, JSYAML_OPTS));
-}
-
-function writeYearIndex(articles, year) {
+function generateYearIndex(metadata) {
+  console.log('Generating index.md files...');
+  var years = Object.keys(metadata);
   var ts = fs.readFileSync('./src/templates/updates-index-year.md', 'utf8');
   var template = Handlebars.compile(ts);
-  articles.sort(compareTocDates);
+  years.forEach(function(year) {
+    console.log(' ', year);
+    var context = {
+      year: year.toString(),
+      articles: metadata[year]
+    };
+    var result = template(context);
+    var filename = path.join('./src/content/en/updates/', year, 'index.md');
+    fs.writeFileSync(filename, result);
+  });
+}
+
+function generateHomePage(metadata) {
+  console.log('Generating Homepage...');
+  var years = Object.keys(metadata);
+  years.sort();
+  var year = years[years.length - 1];
+  var forHomePage = [];
+  if (metadata[year].length > MAX_ARTICLES_ON_HOMEPAGE) {
+    forHomePage = metadata[year].slice(0, MAX_ARTICLES_ON_HOMEPAGE);
+  }
+  var ts = fs.readFileSync('./src/templates/updates-homepage.yaml', 'utf8');
+  var template = Handlebars.compile(ts);
   var context = {
-    year: year.toString(),
-    articles: articles
+    articles: forHomePage,
+    currentYear: year
   };
-  var lang = 'en';
   var result = template(context);
-  var filename = path.join(ROOT_PATH, lang, 'updates', year, 'index.md');
+  var filename = './src/content/en/updates/_index.yaml';
   fs.writeFileSync(filename, result);
 }
 
-function buildYear(lang, year) {
-  var tocFile;
-  var yearToc = [];
-  var abc = [];
-  var sourcePath = path.join(ROOT_PATH, lang, 'updates', year);
-  var months = fs.readdirSync(sourcePath);
-  months.forEach(function(month) {
-    var fileStat = fs.statSync(path.join(sourcePath, month));
-    if (month.length === 2 && fileStat.isDirectory()) {
-      var articles = buildMonth(lang, year, month);
-      if (articles) {
-        tocFile = path.join(ROOT_PATH, lang, 'updates', year, month, '_toc.yaml');
-        var monthName = moment.months()[parseInt(month - 1)];
-        writeToc(articles, monthName, tocFile);
-        var x = '/web/updates/' + year + '/' + month + '/_toc.yaml';
-        yearToc.push({include: x});
-        abc = abc.concat(articles);
-      }
-    }
-  });
-  tocFile = path.join(ROOT_PATH, lang, 'updates', year, '_toc.yaml');
-  writeYearToc(yearToc, year, tocFile);
-  writeYearIndex(abc, year);
+function generateATOM(metadata) {
+  // TODO: NYI
+  console.log('Generating ATOM feed... (NYI)');
 }
 
-function go(lang) {
-  var sourcePath = path.join(ROOT_PATH, lang, 'updates');
-  var years = fs.readdirSync(sourcePath);
-  years.forEach(function(year) {
-    var fileStat = fs.statSync(path.join(sourcePath, year));
-    if (year.length === 4 && year.indexOf(20) === 0 && fileStat.isDirectory()) {
-      buildYear(lang, year);
-    }
-  });
+function generateRSS(metadata) {
+  // TODO: NYI
+  console.log('Generating RSS feed... (NYI)');
 }
 
-var authorYaml = fs.readFileSync('./src/data/_contributors.yaml', 'utf8');
-authorYaml = jsYaml.safeLoad(authorYaml);
-Handlebars.registerHelper('authorName', function(person) {
-  var author = authorYaml[person];
-  if (author) {
-    return author.name.given + ' ' + author.name.family;
-  }
-  return '';
-});
-Handlebars.registerHelper('momentFormat', function(d) {
-  var m = moment(d);
-  return m.format('dddd, MMMM Do YYYY');
-});
+var lang = 'en';
+var fileList = getListOfUpdates(path.join(ROOT_PATH, lang, 'updates'));
+var articles = getAllMetadata(fileList);
+generateHomePage(articles);
+generateYearIndex(articles);
+generateYearTOCs(articles);
+generateATOM(articles);
+generateRSS(articles);
 
-go('en');
