9c2cba45395fea1e8dd5fd92320c017cd959e7c4
Pete LePage <pete.lepage@pobox.com>
2016-10-12 13:45:15 -0400

    Minor cleanup & update

--- tools/update-resources.sh
+++ tools/update-resources.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Getting lastest CSS and Script resources..."
 
-curl https://developers.google.com/_static/css/devsite-google-blue.css > ../gae/styles/devsite-google-blue.css
-curl https://developers.google.com/_static/js/framebox.js > ../gae/scripts/framebox.js
-curl https://developers.google.com/_static/js/jquery_ui-bundle.js > ../gae/scripts/jquery_ui-bundle.js
-curl https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js > ../gae/scripts/jquery-2.1.1.min.js
-curl https://developers.google.com/_static/js/prettify-bundle.js > ../gae/scripts/prettify-bundle.js
-curl https://developers.google.com/_static/js/script_foot_closure.js > ../gae/scripts/footer-closure.js
+curl https://developers.google.com/_static/css/devsite-google-blue.css > gae/styles/devsite-google-blue.css
+curl https://developers.google.com/_static/js/framebox.js > gae/scripts/framebox.js
+curl https://developers.google.com/_static/js/jquery_ui-bundle.js > gae/scripts/jquery_ui-bundle.js
+curl https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js > gae/scripts/jquery-2.1.1.min.js
+curl https://developers.google.com/_static/js/prettify-bundle.js > gae/scripts/prettify-bundle.js
+curl https://developers.google.com/_static/js/script_foot_closure.js > gae/scripts/footer-closure.js
