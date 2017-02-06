6c276d34238c567147e8a24cdd00b66ff06693a3
Pete LePage <pete.lepage@pobox.com>
2017-02-06 10:48:19 -0500

    build script update

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+function installGAE {
+  curl https://sdk.cloud.google.com | bash
+  $HOME/google-cloud-sdk/bin/gcloud components update app -q
+}
+
+function pullRequest {
+  echo "*** PULL REQUEST ***"
+  installGAE
+}
+
+function deployMaster {
+  echo "--- MASTER BUILD ---"
+}
+
+
 echo "Travis Deploy Script"
 
 echo TRAVIS_BRANCH $TRAVIS_BRANCH
+echo TRAVIS_BUILD_ID $TRAVIS_BUILD_ID
 echo TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER
 echo TRAVIS_BUILD_DIR $TRAVIS_BUILD_DIR
 echo TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST
@@ -10,8 +26,8 @@ echo TRAVIS_PULL_REQUEST_BRANCH $TRAVIS_PULL_REQUEST_BRANCH
 echo TRAVIS_EVENT_TYPE $TRAVIS_EVENT_TYPE
 echo TRAVIS_TEST_RESULT $TRAVIS_TEST_RESULT
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "*** TRAVIS PULL REQUEST ***"
+if [ "${TRAVIS_EVENT_TYPE}" = "pull_request" ]; then
+  pullRequest
 else
-  echo "--- NOT A PULL REQUEST ---"
+  deployMaster
 fi
