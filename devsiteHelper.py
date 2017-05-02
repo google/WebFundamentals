5da363d8fe0d21f7efb31633013525acf365bc76
Surma <surma@surmair.de>
2017-05-02 11:20:20 +0100

    Fix `include` implementation (#4498)

--- devsiteHelper.py
+++ devsiteHelper.py
@@ -266,6 +266,7 @@ def getInclude(includeTag, lang='en'):
   fileName = fileName.replace('"', '')
   fileName = fileName.replace('\'', '')
   fileName = fileName.strip()
+  result = None
   if fileName == 'comment-widget.html':
     result = '<style>'
     result += '#gplus-comment-container { border: 1px solid #c5c5c5; }'
@@ -374,7 +375,7 @@ def getAnnouncementBanner(lang='en'):
 
 
 def getFooterPromo(lang='en'):
-  """Gets the promo footer. 
+  """Gets the promo footer.
 
   Args:
       lang: The language to pick from.
@@ -407,7 +408,7 @@ def getFooterPromo(lang='en'):
 
 
 def getFooterLinkBox(lang='en'):
-  """Gets the promo boxes. 
+  """Gets the promo boxes.
 
   Args:
       lang: The language to pick from.
