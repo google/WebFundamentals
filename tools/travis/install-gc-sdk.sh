3586137ccda4652894b52ae9da918a5653c4e06c
Pete LePage <pete.lepage@pobox.com>
2017-06-26 12:59:39 -0400

    wip

--- tools/travis/install-gc-sdk.sh
+++ tools/travis/install-gc-sdk.sh
@@ -14,13 +14,11 @@ fi
 # Decrypt the Service Account Key
 openssl aes-256-cbc -K $encrypted_7c1b14c4fe42_key -iv $encrypted_7c1b14c4fe42_iv -in gcloud-client-secret.json.enc -out gcloud-client-secret.json -d
 
-ls $HOME/google-cloud-sdk/
-
-if [ ! -f /home/travis/google-cloud-sdk/bin/gcloud ]; then
-  rm -rf /home/travis/google-cloud-sdk
+if [ ! -f "$HOME/google-cloud-sdk/bin/gcloud" ]; then
+  # Remove the automatically created directory
+  rm -rf $HOME/google-cloud-sdk
   # Download & install the Google Cloud SDK
   curl https://sdk.cloud.google.com | bash;
-  ls $HOME/google-cloud-sdk
 fi
 
 # Update any necessary components
