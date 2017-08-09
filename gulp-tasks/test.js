253abf67be4ab06c06720e1e6248519b3c9d07e6
Pete LePage <pete.lepage@pobox.com>
2017-08-09 10:30:33 -0700

    only fail on real scripts not samples (#4930)

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -649,7 +649,7 @@ function testMarkdown(filename, contents, options) {
     });
 
     // Error on script blocks in markdown
-    matched = wfRegEx.getMatches(/<\/?script/gm, contents);
+    matched = wfRegEx.getMatches(/^<script/gm, contents);
     matched.forEach(function(match) {
       position = {line: getLineNumber(contents, match.index)};
       msg = 'Unescaped script tag in markdown document, use &lt;script...';
