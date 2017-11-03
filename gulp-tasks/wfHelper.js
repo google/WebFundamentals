3df230b6b16d94897306274132d0f9f587067bc3
Pete LePage <pete.lepage@pobox.com>
2017-11-03 12:35:37 -0400

    Compare titles with case insensitivity

--- gulp-tasks/wfHelper.js
+++ gulp-tasks/wfHelper.js
@@ -93,6 +93,12 @@ function promisedRSync(src, dest) {
 }
 
 function genericComparator(a, b) {
+  if (typeof a === 'string') {
+    a = a.toLowerCase();
+  }
+  if (typeof b === 'string') {
+    b = b.toLowerCase();
+  }
   if (a < b) {
     return 1;
   } else if (a > b) {
