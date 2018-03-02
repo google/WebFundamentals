7b38f8eaa898028a7812fa928f846c0e4a91ef6e
Luca Berton <mr.evolution85@gmail.com>
2018-03-02 16:57:14 +0100

    Integrated @petele 's tips

--- gulp-tasks/wfContributors.js
+++ gulp-tasks/wfContributors.js
@@ -82,11 +82,11 @@ function buildIndex(contributors) {
 function buildIndividualPages(contributors) {
   gutil.log(' ', 'Building individual pages of all contributors...');
   global.WF.langs.forEach(function(lang) {
-    const folder = global.WF.src.content.replace('/en', '/'.concat(lang));
+    const folder = global.WF.src.content.replace('/en', `/${lang}`);
     const files = wfHelper.getFileList(folder, ['**/*.md']);
     const filesByAuthor = wfHelper.splitByAuthor(files);
     const keys = Object.keys(contributors);
-    keys.forEach(function(key) {
+    keys.forEach((key) => {
       if (!(key in filesByAuthor)) {
         return;
       }
