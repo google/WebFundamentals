db6419722375f9abd6d8eb29533620f50746abf3
Jimmy Casey <casey.jimmy@gmail.com>
2018-07-26 19:57:57 +0000

    Fixed Spelling.

--- gulp-tasks/tests/validateHtml.js
+++ gulp-tasks/tests/validateHtml.js
@@ -54,7 +54,7 @@ function test(filename, contents, options) {
    * Simple wrapper that adds an error to the list
    *
    * @param {string} message The message to add.
-   * @param {object} position The line number the error occured on.
+   * @param {object} position The line number the error occurred on.
    */
   function logError(message, position) {
     results.push({
