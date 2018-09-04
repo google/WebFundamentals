501bd4fb070cd79a34418b843589078b46009ce8
Pete LePage <pete.lepage@pobox.com>
2018-09-04 12:43:18 -0400

    New in Chrome 69 (#6576)
    * NIC69 post
    
    * Support for expandable elements
    
    * More devsite cleanup
    
    * Adds /web/updates/nic redirect to latest
    
    * final updates

--- tools/update-resources.sh
+++ tools/update-resources.sh
@@ -17,8 +17,9 @@ echo "Getting lastest CSS and Script resources..."
 
 getResource https://developers.google.com/_static/css/devsite-google-blue.css gae/styles/devsite-google-blue.css
 getResource https://developers.google.com/_static/css/devsite-orange.css gae/styles/devsite-orange.css
-getResource https://developers.google.com/_static/js/script_foot_closure.js gae/scripts/footer-closure.js
-getResource https://developers.google.com/_static/js/framebox.js gae/scripts/framebox.js
-getResource https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js gae/scripts/jquery-2.1.1.min.js
-getResource https://developers.google.com/_static/js/prettify-bundle.js gae/scripts/prettify-bundle.js
 
+getResource https://developers.google.com/_static/js/framebox.js gae/scripts/framebox.js
+getResource https://developers.google.com/_static/js/jquery-bundle.js gae/scripts/jquery-bundle.js
+getResource https://developers.google.com/_static/js/script_foot_closure.js gae/scripts/script_foot_closure.js
+getResource https://developers.google.com/_static/js/script_foot.js gae/scripts/script_foot.js
+getResource https://developers.google.com/_static/js/prettify-bundle.js gae/scripts/prettify-bundle.js
