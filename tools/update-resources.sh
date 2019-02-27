1a4da1728a8503b37c92f5d951b4b20c570ae8d9
Nathan West <Lucretiel@users.noreply.github.com>
2019-02-27 08:54:55 -0800

    Parallelize update-resources.sh (#7117)
    * Parallelize update-resources.sh
    
    * Removed progress bar
    
    * change to silent

--- tools/update-resources.sh
+++ tools/update-resources.sh
@@ -3,21 +3,24 @@
 getResource() {
   local TEMP_FILE="$(mktemp)"
   echo "$1"
-  if curl --compressed --fail --progress-bar "$1" > "$TEMP_FILE"; then
-      mv "$TEMP_FILE" "$2"
+  if curl --compressed --fail --silent "$1" > "$TEMP_FILE"; then
+    mv "$TEMP_FILE" "$2"
   else
-      echo Unable to update "$1"
-      rm "$TEMP_FILE"
+    echo Unable to update "$1"
+    rm "$TEMP_FILE"
   fi
 }
 
 echo "Getting lastest CSS and Script resources..."
 
-getResource https://developers.google.com/_static/css/devsite-google-blue.css gae/styles/devsite-google-blue.css
-getResource https://developers.google.com/_static/css/devsite-orange.css gae/styles/devsite-orange.css
+getResource https://developers.google.com/_static/css/devsite-google-blue.css gae/styles/devsite-google-blue.css &
+getResource https://developers.google.com/_static/css/devsite-orange.css gae/styles/devsite-orange.css &
 
-getResource https://developers.google.com/_static/js/framebox.js gae/scripts/framebox.js
-getResource https://developers.google.com/_static/js/jquery-bundle.js gae/scripts/jquery-bundle.js
-getResource https://developers.google.com/_static/js/script_foot_closure.js gae/scripts/script_foot_closure.js
-getResource https://developers.google.com/_static/js/script_foot.js gae/scripts/script_foot.js
-getResource https://developers.google.com/_static/js/prettify-bundle.js gae/scripts/prettify-bundle.js
+getResource https://developers.google.com/_static/js/framebox.js gae/scripts/framebox.js &
+getResource https://developers.google.com/_static/js/jquery-bundle.js gae/scripts/jquery-bundle.js &
+getResource https://developers.google.com/_static/js/script_foot_closure.js gae/scripts/script_foot_closure.js &
+getResource https://developers.google.com/_static/js/script_foot.js gae/scripts/script_foot.js &
+getResource https://developers.google.com/_static/js/prettify-bundle.js gae/scripts/prettify-bundle.js &
+
+# TODO: return an error code if one of the curls fails
+wait
