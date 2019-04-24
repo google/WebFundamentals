180c4e83042fec881e20fef3e8ab3c62ca476338
Pete LePage <pete.lepage@pobox.com>
2019-04-24 17:55:58 +0200

    Updated translate script
    Signed-off-by: Pete LePage <pete.lepage@pobox.com>

--- tools/translate.sh
+++ tools/translate.sh
@@ -1,9 +1,11 @@
-#! /usr/bin/bash
+#!/bin/bash
+
 export GOOGLE_APPLICATION_CREDENTIALS=../../key.json
-find ../src/content/en/updates/2018/01/ -iname "*.md" |
+find ../src/content/en/fundamentals/codelabs/your-first-pwapp/ -iname "*.md" |
 while read filename
-do 
-  node translate.js -s $filename -t hi,fr,es,ja,de,vi,ru,id  \;
+do
+  node translate.js -s $filename -t ar,de,es,fr,hi,id,ja,ko,pt-br,ru,vi,zh-cn,zh-tw  \;
 done
 
+#ar,de,es,fr,hi,id,ja,ko,pt-br,ru,vi,zh-cn,zh-tw
 #find src/content/en/updates/2018/01/ -iname "*.md" -exec node translate.js -s '{}' -t hi,fr,es,ja,de,vi,ru,id \;
